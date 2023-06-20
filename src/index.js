cea18d3e1ac8182ece8eb2f0c02d0a83aff6c345
ZorAnderius <gabriel.zorander@gmail.com>
2023-06-20 22:02:23 +0100

    add dependencies

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './data/user.json';
+
+const { username, tag, location, avatar, stats } = user;
+
+const userInfo = (
+  <div className="profile">
+    <div className="description">
+      <img src={avatar} alt={username} className="avatar" />
+      <p className="name">{username}</p>
+      <p className="tag">@{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  userInfo
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
